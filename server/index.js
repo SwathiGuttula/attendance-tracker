23311103db6d5acf65a3ccddbe55aa0bd1b3f32e
SwathiGuttula <swathisree.guttula@gmail.com>
2026-06-12 10:50:24 +0530

    fix CORS for production

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const initDB  = require('./config/initDB')
 
 const app = express()
 
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000'] }))
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 
 // Routes
